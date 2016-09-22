6b978bdfecebdcc4e6aa83b7713f4800709638e5
Georgios Psarakis <g.psarakis3800@gmail.com>
2016-09-22 18:56:25 -0400

    lesson 3 in class splash screen

---
+++ COMP1004-F2016-Lesson3/COMP1004-F2016-Lesson3/SplashForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP1004_F2016_Lesson3
+{
+    public partial class SplashForm : Form
+    {
+        public SplashForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SplashFormTimer_Tick(object sender, EventArgs e)
+        {   // if not set to false it will keep creating a splash form ever 3 seconds 
+            SplashFormTimer.Enabled = false;
+
+            StartForm startForm = new StartForm();
+            startForm.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ COMP1004-F2016-Lesson3/COMP1004-F2016-Lesson3/StartForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace COMP1004_F2016_Lesson3
+{
+    public partial class StartForm : Form
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+        }
+
+        private void ClickMeButton_Click(object sender, EventArgs e)
+        {
+            MessageLabel.Text = (MessageLabel.Text == "Hello") ? "Good Bye!" : "Hello";
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            // Instantiate the next form
+            NextForm nextForm = new NextForm();
+            // save a refernece to the current form in a property of the nextForm
+            nextForm.previousForm = this;
+
+            // show the next form
+            nextForm.Show();
+
+            //hide this one
+            this.Hide();
+        }
+
+        private void StartForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit(); 
+        }
+    }
+}
